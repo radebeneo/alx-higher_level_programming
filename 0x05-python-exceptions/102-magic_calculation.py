ba84ad7607dbf7c08eee98414db27819948343ce
radebeneo <radebeneo17@gmail.com>
2024-10-01 11:22:23 -0700

    ByteCode -> Python #4

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    result = 0
+    i = 1
+
+     while i < 3:
+         try:
+             if i > a:
+                 raise Exception("Too far")
+             else:
+                 result += a ** b / i
+         except Exception:
+             result = b + a
+             break
+
+     i += 1
+
+     return result
